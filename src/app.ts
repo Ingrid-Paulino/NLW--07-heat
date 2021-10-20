53e9dc191c27ab8dbae9015ad8d599a973a5127d
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-10-20 12:33:42 -0300

    C0 - Criaçao do projeto + intalaçao do Express e do typescript

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import express from "express";
+
+const app = express();
+
+app.listen(4000,() => console.log(':rocketseat - Server is running on PORT 4000')) 
+// porta que a minha aplicação vai rodar, o console é so pra saber que a aplicação esta rodando
